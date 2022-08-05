0961651fc224f2ebff45ec6d98a36f941cfe52af
上傳人名字 <flamesky823@gmail.com>
2022-08-05 16:35:44 +0800

    update

--- test-0config.sh
+++ test-0config.sh
@@ -4,13 +4,13 @@ Team=00		# 自己隊伍的編號
 You=00		# 攻擊方隊伍的編號
 python=python3
 Csv=Csv
-B=$Csv/B.csv
-C=$Csv/C.csv
-X=$Csv/pre_anony_${Team}_x.csv
-D=$Csv/pre_anony_${Team}_d.csv
-CT=$Csv/pre_anony_${Team}_c.csv 
-EA=$Csv/pre_anony_${Team}_ea.csv
-E=$Csv/pre_attack_${You}_from_${Team}.csv
+B=anony/${Team}/$Csv/B.csv
+C=anony/${Team}/$Csv/C.csv
+X=anony/${Team}/$Csv/pre_anony_${Team}_x.csv
+D=anony/${Team}/$Csv/pre_anony_${Team}_d.csv
+CT=anony/${Team}/$Csv/pre_anony_${Team}_c.csv 
+EA=anony/${Team}/$Csv/pre_anony_${Team}_ea.csv
+E=attack/${You}/$Csv/pre_attack_${You}_from_${Team}.csv
 
 pytest(){
 	echo $*
