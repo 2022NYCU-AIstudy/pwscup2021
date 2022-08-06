ea989b84f5d4fde2b31a1e0673f79defe083cc02
SkyLiNing823 <84125881+SkyLiNing823@users.noreply.github.com>
2022-08-06 19:44:15 +0800

    Update test-2anonymize.sh

--- test-2anonymize.sh
+++ test-2anonymize.sh
@@ -2,7 +2,7 @@
 
 source test-0config.sh
 
-# 第1匿名化: 適用top, bottom, k-匿名，目的為刪除列
+# 第1去識別化: 適用top, bottom, k-匿名，目的為刪除列
 pytest main/anony_phase/first-anony/top2.py $B 1_5 75_50 anony/${Team}/$Csv/e-top.csv
 pytest main/anony_phase/first-anony/bottom2.py $B 1_5 22_20 anony/${Team}/$Csv/e-bot.csv
 pytest main/anony_phase/first-anony/kanony2.py $B 7 2_3_4  anony/${Team}/$Csv/e-ka.csv
@@ -15,7 +15,7 @@ pytest main/anony_phase/first-anony/exclude.py $B $X $C anony/${Team}/$Csv/e-in2
 pytest main/anony_phase/mark/umark.py $B $C
 pytest main/anony_phase/mark/uniqrt.py  $C
 
-# 第2匿名化: 使用rr, dp, quantify，不可刪除行
+# 第2去識別化: 使用rr, dp, quantify，不可刪除行
 pytest main/anony_phase/second-anony/rr.py $C  0.9 anony/${Team}/$Csv/d-xrr2.csv  0_2_3_4_6_7_10
 pytest main/anony_phase/second-anony/dp2.py anony/${Team}/$Csv/d-xrr2.csv 1_5 1.0_2.0 $D
 pytest main/anony_phase/mark/umark.py $B $D
